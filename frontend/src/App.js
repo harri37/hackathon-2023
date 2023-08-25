5e105f7d41babfdad56a69e377abbe595077be59
harri37 <harrison.wills@outlook.com>
2023-08-25 20:25:45 +1000

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import Home from "./pages/Home";
+import Game from "./pages/Game";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/game" element={<Game />} />
+      <Route path="*" element={<h1>Not Found</h1>} />
+    </Routes>
+  );
 }
 
 export default App;
