0edbc78c6165720b45b109ea408cb4f0696a1ea2
harri37 <harrison.wills@outlook.com>
2023-08-25 20:03:38 +1000

    added home page

---
+++ frontend/src/images/images.js
@@ -0,0 +1,3 @@
+import home from "./home.jpg";
+
+export const homeImage = home;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { homeImage } from "../images/images";
+
+function Home() {
+  function Title() {
+    return <h1 className="Title">Home</h1>;
+  }
+
+  return <div className="Home"></div>;
+}
+
+export default Home;
