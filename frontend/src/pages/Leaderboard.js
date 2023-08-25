4f8e5ed1c77f2a3e03254a42d34ac0ccaaeb48db
hugh5 <hughdrummond11@gmail.com>
2023-08-25 23:26:00 +1000

    added columsn to leaderboard with test data

--- frontend/src/pages/Leaderboard.js
+++ frontend/src/pages/Leaderboard.js
@@ -1,31 +1,97 @@
-import { Button, Card } from "react-bootstrap";
+import { rankUpImage, rankDownImage, stayImage } from "../images/images";
 
 function Leaderboard() {
     return (
-        <div>
+        <div style={{ padding: "10px 40px" }}>
             <h1>Leaderboard</h1>
-            {/* Styled card */}
-            <Card style={{ width: "18rem" }}>
-                <Card.Body>
-                    <Card.Title>Leaderboard</Card.Title>
-                    <Row name="John" score="100" />
-                </Card.Body>
-            </Card>
+
+            <table className="leaderboardTable">
+                <thead>
+                    <tr>
+                        <th scope="col" style={{ width: "10%" }}>
+                            Rank
+                        </th>
+                        <th scope="col" style={{ width: "45%" }}>
+                            Name
+                        </th>
+                        <th scope="col" style={{ width: "15%" }}>
+                            Height
+                        </th>
+                        <th scope="col" style={{ width: "15%" }}>
+                            Nationality
+                        </th>
+                        <th scope="col" style={{ width: "15%" }}>
+                            Skill Rating
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {testData.map((data, index) => (
+                        <tr key={index}>
+                            <td>
+                                <img
+                                    style={{
+                                        marginRight: "8px",
+                                        marginBottom: "-5px",
+                                    }}
+                                    src={
+                                        data.yesterdayIndex > index
+                                            ? rankUpImage
+                                            : data.yesterdayIndex < index
+                                            ? rankDownImage
+                                            : stayImage
+                                    }
+                                    height="20px"
+                                />
+                                {index + 1}
+                            </td>
+                            <td>{data.username}</td>
+                            <td>{data.height}</td>
+                            <td>
+                                <img
+                                    height={32}
+                                    src={`https://flagsapi.com/${data.nationality}/flat/64.png`}
+                                    // style={{ border: "1px solid black" }}
+                                />
+                            </td>
+                            <td>{data.skillRating}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
 
-const Row = ({ name, score }) => {
-    return (
-        <div>
-            <table>
-                <tr>
-                    <td>{name}</td>
-                    <td>{score}</td>
-                </tr>
-            </table>
-        </div>
-    );
-};
+const testData = [
+    {
+        username: "Username",
+        nationality: "AU",
+        height: "6'3",
+        skillRating: 4320,
+        yesterdayIndex: 2,
+    },
+    {
+        username: "Username2",
+        nationality: "GR",
+        height: "6'3",
+        skillRating: 4317,
+        yesterdayIndex: 1,
+    },
+    {
+        username: "Username3",
+        nationality: "SC",
+        height: "6'2",
+        skillRating: 4294,
+        yesterdayIndex: 0,
+    },
+    {
+        username: "Username4",
+        nationality: "CA",
+        height: "6'1",
+        skillRating: 4246,
+        yesterdayIndex: 3,
+    },
+];
 
 export default Leaderboard;
